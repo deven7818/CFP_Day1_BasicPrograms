00f10ca13c8c27094c13538407543dc4e49d34b7
deven7818 <deven7818@gmail.com>
2022-04-22 23:12:10 +0530

    Added Arrow function example

---
+++ ArrowFunction/ArrowFunction.php
@@ -0,0 +1,12 @@
+<?php
+
+//Normal function
+$count  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// $result = array_map(function($n){
+//     return $n * 10;
+// },$count);
+
+//Arrow function
+$result = array_map(fn ($n) => $n * 15, $count);
+print_r($result);
+?> 
