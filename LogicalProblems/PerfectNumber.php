941d6ea76be5cf9d3888156ab3615fb324dbfeee
deven7818 <deven7818@gmail.com>
2022-04-22 23:13:07 +0530

    Added Logical problems

---
+++ LogicalProblems/PerfectNumber.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Program to check wheather given number is perfect number or not
+ */
+class PerfectNumber
+{
+    //function to check perfect number
+    function perfectNum($num)
+    {
+        $sum = 0;
+        for ($i = 1; $i < $num; $i++) {
+            if (($num % $i) == 0) {
+                $sum = $sum + $i;
+            }
+        }
+        if ($sum == $num) {
+            echo "The Number $num is perfect Number.";
+        } else {
+            echo "The number $num is not perfect Number";
+        }
+    }
+}
+//creating object of class
+$perfectNum = new PerfectNumber();
+$num = readline("Enter a number : ");
+$perfectNum->perfectNum($num);
+?> 
---
+++ LogicalProblems/ReverseNumber.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Program for Reverse the given number
+ */
+class ReverseNumber
+{
+    //function to reverse the number
+    function reverse($num)
+    {
+        $reverse = 0;
+        while ($num > 0) {
+            $rem = $num % 10;
+            $reverse = ($reverse * 10) + $rem;
+            $num = floor($num / 10);
+        }
+        return $reverse;
+    }
+}
+//creating object of class
+$reverseNum = new ReverseNumber();
+$num = readline("Enter the number :");
+echo "The reverse of given  $num is " . $reverseNum->reverse($num);
+?> 
